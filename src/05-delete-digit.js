8697f947efc9855310f551c03c6144d32f4331c0
Insikynwa <pavel.churkin@mail.ru>
2021-04-12 01:15:21 +0300

    feat: implement deleteDigit function

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,13 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const newArray = String(n).split('');
+  const minNumber = Math.min(...newArray);
+  const indexMinNumber = newArray.indexOf(String(minNumber));
+  newArray.splice(indexMinNumber, 1);
+  const newNumber = newArray.join('');
+  return +newNumber;
 }
 
 module.exports = deleteDigit;
